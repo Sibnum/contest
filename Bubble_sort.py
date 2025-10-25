113c02b611e0c08600c59032345b8274af528675
Igor Zinchenko <sibnum@yandex.ru>
2025-10-25 10:49:12 +0300

    tast is done

--- Bubble_sort.py
+++ Bubble_sort.py
@@ -4,8 +4,9 @@ example_array = [6, 5, 3, 1, 8, 7, 2, 4]
 def bubble_sort(data):
     last_index = len(data) - 1
     item_index = 0
-    swapped = False
+    swapped = True
     while last_index != 1 or swapped is True:
+        swapped = False
         for i in range(last_index):
             if data[item_index] > data[item_index + 1]:
                 data[item_index], data[item_index + 1] = (
@@ -13,12 +14,15 @@ def bubble_sort(data):
                 )
                 swapped = True
                 item_index += 1
-            if swapped is True:
-                last_index -= 1
-                continue
             else:
-                swapped is False
-                break
+                item_index = item_index + 1
+        if swapped is True:
+            last_index -= 1
+            item_index = 0
+            continue
+        else:
+            swapped is False
+            break
     return data
 
 
