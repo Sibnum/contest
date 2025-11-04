8b10cf5b6a2d24481810073d5270393843cdbc1b
Igor Zinchenko <sibnum@yandex.ru>
2025-11-04 11:20:26 +0300

    task is done

--- pattern-base_sorting.py
+++ pattern-base_sorting.py
@@ -1,13 +1,21 @@
 def main(n: int, m: int, array_n: list[int], array_m: list[int]) -> str:
     result = []
-    last_digit = []
-    for i_m in range(m):  # итерируем по кол-ву цифр
+    for i_m in range(m):
         current_m = array_m[i_m]
         for i_n in range(n):
             current_n = array_n[i_n]
             if current_n == current_m:
                 result.append(current_n)
-        if current_n != current_m:  # переписать на цикл с индекса array_m + 1
+    last_digit = []
+    for i_n in range(n):
+        current_n = array_n[i_n]
+        found = False
+        for i_m in range(m):
+            current_m = array_m[i_m]
+            if current_n == current_m:
+                found = True
+                break
+        if not found:
             last_digit.append(current_n)
     last_digit.sort()
     result.extend(last_digit)
@@ -15,10 +23,10 @@ def main(n: int, m: int, array_n: list[int], array_m: list[int]) -> str:
 
 
 if __name__ == '__main__':
-    n: int = int(input())  # кол-во чисел, которые надо отсортировать
-    array_n: str = input()  # строка с n чисел, которые надо отсортировать
-    m: int = int(input())  # кол-во чисел в шаблоне
-    array_m: str = input()  #строка с m чисел - шаблон
-    array_n: list = [int(digit) for digit in array_n.split()]  #список для сорт
-    array_m: list = [int(digit) for digit in array_m.split()]  #список-шаблон
+    n: int = int(input())
+    array_n_str: str = input()
+    m: int = int(input())
+    array_m_str: str = input()
+    array_n: list = [int(digit) for digit in array_n_str.split()]
+    array_m: list = [int(digit) for digit in array_m_str.split()]
     print(main(n, m, array_n, array_m))
