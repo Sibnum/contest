454c3280fbbd962e4580d3730573622f6a279fdd
Igor Zinchenko <sibnum@yandex.ru>
2025-11-07 21:11:48 +0300

    task is done

--- encrypted_instructions.py
+++ encrypted_instructions.py
@@ -1,4 +1,24 @@
+# ID 147834521
+"""Шифрованные инструкции (Encrypted_instructions).
+
+Программа расшифровывает сжатые сообщения и возвращает строку с командами.
+
+Основные компоненты:
+
+**command** - ввод сокращённой формы команды. В строке могут быть только числа,
+буквы и квадратные скобки.
+
+**decoding** - функция, декодирующая сокращённую форму команды в полную форму.
+"""
+
+
 def decoding(command: str) -> str:
+    """Декодирует сокращённую форму команды в полную форму.
+
+    На вход принимает строку с сокращённой формой команды. Алгоритм
+    декодирования использует стэк.
+    Возвращает строку с полной формой команды.
+    """
     stack: list = []
     current_string: str = ''
     current_number: str = ''
@@ -10,8 +30,8 @@ def decoding(command: str) -> str:
         elif symbol == '[':
             stack.append(int(current_number))
             stack.append(current_string)
-            current_number: str = ''
-            current_string: str = ''
+            current_number = ''
+            current_string = ''
         elif symbol == ']':
             previous_string: str = stack.pop()
             repeat_number: int = stack.pop()
